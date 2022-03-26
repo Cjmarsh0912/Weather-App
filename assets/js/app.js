2a91e137adbf68d1c46aa7b7899aaee382250da2
Cjmarsh0912 <75540207+Cjmarsh0912@users.noreply.github.com>
2022-03-26 14:40:25 -0400

    Added the abilty to display basic weather info

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-/*let unix_timestamp = 647184023
+/*let unix_timestamp = 1647291960;
 
 var date = new Date(unix_timestamp * 1000);
 var hours = date.getHours();
@@ -7,37 +7,58 @@ var seconds = "0" + date.getSeconds();
 
 var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
 
-console.log(formattedTime);
-https://api.openweathermap.org/data/2.5/weather?lat=35.9603948&lon=-83.9210261&units=imperial&appid=b18a17f280f5e12915af91620df8b8c4
-*/
+console.log(formattedTime);*/
+
+
+
 let $key = 'b18a17f280f5e12915af91620df8b8c4',
-    $city = $('.city'),
+    $cityInput = $('.city-input'),
     $submit = $('.submit'),
-    $name = $('.name'),
-    $wind = $('.wind'),
-    $temp = $('.temp');
+    $name = $('.city-name'),
+    $temp = $('.temp'),
+    $wind = $('.wind-speed'),
+    $lat = '',
+    $lon = '',
+    $state = '';
+
+/*fetch('https://api.openweathermap.org/data/2.5/onecall?lat=35.9604&lon=-83.921&units=imperial&exclude=daily,current,minutely,alerts&appid=b18a17f280f5e12915af91620df8b8c4')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        var date = new Date(data['hourly'][3]['dt'] * 1000);
+        console.log(date);
+    })*/
 
 $submit.click(() => {
-    fetch('https://api.openweathermap.org/geo/1.0/direct?q=' + $city.val() + '&appid=' + $key).then(response => response.json()).then(data => {
-        console.log(data);
-        fetch('https://api.openweathermap.org/data/2.5/weather?lat=' +
-            data[0]['lat'] + '&lon=' + data[0]['lon'] + '&units=imperial&appid=' + $key)
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-                let $cityTemp = data['main']['temp'];
-                let $windSpeed = data['wind']['speed'];
-                let $cityName = data['name'];
+    $name.empty();
+    $temp.empty();
+    $wind.empty();
+    fetch('https://api.openweathermap.org/geo/1.0/direct?q=' + $cityInput.val() + '&appid=' + $key)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            $state = data[0]['state'];
+            $lon = data[0]['lon'];
+            $lat = data[0]['lat'];
 
-                $name.text($cityName);
-                $wind.text($windSpeed);
-                $temp.text($cityTemp);
+            fetch('https://api.openweathermap.org/data/2.5/weather?lat=' +
+                $lat + '&lon=' + $lon + '&units=imperial&appid=' + $key)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                    let $cityName = data['name'],
+                        $cityTemp = data['main']['temp'].toFixed(0),
+                        $feelsLike = data['main']['feels_like'].toFixed(0),
+                        $windSpeed = data['wind']['speed'].toFixed(0);
 
+                    $name.append($cityName + '<br>' + '<span>' + $state + '</span>');
+                    $temp.append($cityTemp + '&#176;');
+                    $wind.append('feels like: ' + $feelsLike + '&#176;<br>wind speed: '
+                        + $windSpeed + '<span>mph</span>');
 
-            }) // end fetch
-
-            .catch(err => alert(err));
-    }) // end fetch
+                }) // end fetch
+                .catch(err => alert(err));
+        }) // end fetch
 
         .catch(err => alert('Not a city name'));
 }); // end click
